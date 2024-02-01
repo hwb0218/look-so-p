76db30781ddd33a5195cc381f9a11fab6d7e90df
FEWoobIn <hwb0218@gmail.com>
2024-02-01 16:57:47 +0900

    faet: mutiple file 조건을 추가합니다

--- src/utils/get-image-data.ts
+++ src/utils/get-image-data.ts
@@ -1,5 +1,17 @@
-export default function getImageData(event: React.ChangeEvent<HTMLInputElement>) {
+export default function getImageData({
+  event,
+  prevImages,
+  multiple = false,
+}: {
+  event: React.ChangeEvent<HTMLInputElement>;
+  prevImages?: FileList;
+  multiple?: boolean;
+}) {
   const dataTransfer = new DataTransfer();
+
+  if (multiple && prevImages) {
+    Array.from(prevImages).forEach((image) => dataTransfer.items.add(image));
+  }
 
   Array.from(event.target.files!).forEach((image) => dataTransfer.items.add(image));
 
