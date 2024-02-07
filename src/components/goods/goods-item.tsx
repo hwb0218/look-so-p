399f9870d5476c2ed3eb007563c0b3f0906ac5a2
FEWoobIn <hwb0218@gmail.com>
2024-02-08 06:09:24 +0900

    feat: 랜딩페이지 상품정보에 캐러셀 슬라이더를 추가합니다(#8)

--- src/components/goods/goods-item.tsx
+++ src/components/goods/goods-item.tsx
@@ -1,8 +1,11 @@
+import { Carousel, CarouselContent, CarouselItem } from '@components/ui/carousel';
+import { Card, CardContent } from '@components/ui/card';
 import { Li } from '@components/common/list';
-
 import Wrapper from '@components/common/wrapper';
 
 import { Product } from '@src/lib/firebase/types';
+
+import numberFormat from '@src/utils/number-format';
 
 interface Props {
   goods: Product;
@@ -11,14 +14,26 @@ interface Props {
 export default function GoodsItem({ goods }: Props) {
   return (
     <Li className="w-1/4">
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {[goods.thumbnail, ...goods.images].map((src) => (
+            <CarouselItem key={src}>
+              <Wrapper className="p-1">
+                <Card>
+                  <CardContent className="p-0 flex aspect-square items-center justify-center">
+                    <img src={src} alt={src} className="w-full h-full object-cover" />
+                  </CardContent>
+                </Card>
+              </Wrapper>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
       <Wrapper>
-        <div>
-          <img src={goods.thumbnail} alt={goods.title} className="w-full" />
-        </div>
         <span>{`[${goods.productCategory}]`}</span>
-        <span>{goods.productPrice}</span>
+        <span>{goods.productName}</span>
       </Wrapper>
-      <div>{goods.productCategory}</div>
+      <div>{`${numberFormat(Number(goods.productPrice))}원`}</div>
     </Li>
   );
 }
