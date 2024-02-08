d86aa7a48af2a5206e7afa6ca7b7567484196eb6
FEWoobIn <hwb0218@gmail.com>
2024-02-08 18:40:46 +0900

    feat: 랜딩 페이지 UI 변경(#8)

--- src/components/goods/goods-item.tsx
+++ src/components/goods/goods-item.tsx
@@ -1,11 +1,11 @@
 import { Carousel, CarouselContent, CarouselItem } from '@components/ui/carousel';
 import { Card, CardContent } from '@components/ui/card';
-import { Li } from '@components/common/list';
 import Wrapper from '@components/common/wrapper';
 
 import { Product } from '@src/lib/firebase/types';
 
 import numberFormat from '@src/utils/number-format';
+import { Li } from '@components/common/list';
 
 interface Props {
   goods: Product;
@@ -13,27 +13,29 @@ interface Props {
 
 export default function GoodsItem({ goods }: Props) {
   return (
-    <Li className="w-1/4">
-      <Carousel className="w-full max-w-xs">
-        <CarouselContent>
-          {[goods.thumbnail, ...goods.images].map((src) => (
-            <CarouselItem key={src}>
-              <Wrapper className="p-1">
-                <Card>
-                  <CardContent className="p-0 flex aspect-square items-center justify-center">
-                    <img src={src} alt={src} className="w-full h-full object-cover" />
-                  </CardContent>
-                </Card>
-              </Wrapper>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
-      <Wrapper>
-        <span>{`[${goods.productCategory}]`}</span>
-        <span>{goods.productName}</span>
-      </Wrapper>
-      <div>{`${numberFormat(Number(goods.productPrice))}원`}</div>
+    <Li className="flex">
+      <div>
+        <Carousel className="w-full max-w-xs">
+          <CarouselContent>
+            {[goods.thumbnail, ...goods.images].map((src) => (
+              <CarouselItem key={src}>
+                <Wrapper className="p-1">
+                  <Card>
+                    <CardContent className="p-0 flex aspect-square items-center justify-center">
+                      <img src={src} alt={src} className="w-full h-full object-cover" />
+                    </CardContent>
+                  </Card>
+                </Wrapper>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <Wrapper>
+          <span>{`[${goods.productCategory}]`}</span>
+          <span>{goods.productName}</span>
+        </Wrapper>
+        <div>{`${numberFormat(Number(goods.productPrice))}원`}</div>
+      </div>
     </Li>
   );
 }
--- src/components/goods/goods-list.tsx
+++ src/components/goods/goods-list.tsx
@@ -1,20 +1,46 @@
+import { createSearchParams, useNavigate } from 'react-router-dom';
+
 import GoodsItem from './goods-item';
 
 import { Ul } from '@components/common/list';
 
+import { ROUTE_PATHS } from '@constants/routes';
+
 import { Product } from '@src/lib/firebase/types';
+import { GoodsCategories } from './types';
 
 interface Props {
+  categories: GoodsCategories;
   goodsByCategory?: {
     [category: string]: Product[];
   };
-  filterCategory: string;
 }
 
-export default function GoodsList({ goodsByCategory, filterCategory }: Props) {
+export default function GoodsList({ categories, goodsByCategory }: Props) {
+  const navigate = useNavigate();
+
+  const handleClickMore = (category: string) => {
+    navigate({
+      pathname: ROUTE_PATHS.GOODS_LIST,
+      search: `?${createSearchParams({ category: category.trim() })}`,
+    });
+  };
+
   return (
-    <Ul className="justify-center gap-x-4 p-3">
-      {goodsByCategory && goodsByCategory[filterCategory]?.map((goods) => <GoodsItem key={goods.id} goods={goods} />)}
-    </Ul>
+    <article className="flex-col items-start p-3">
+      {categories.map(({ value, title }) => (
+        <section key={value} className="mb-10">
+          <div className="relative">
+            <h3 className="pb-10 text-2xl text-center">{title}</h3>
+            <span className="absolute top-0 right-0 cursor-pointer" onClick={() => handleClickMore(value)}>
+              + More
+            </span>
+          </div>
+          <Ul className="gap-x-6">
+            {goodsByCategory && goodsByCategory[value]?.map((goods) => <GoodsItem key={goods.id} goods={goods} />)}
+          </Ul>
+        </section>
+      ))}
+    </article>
   );
 }
