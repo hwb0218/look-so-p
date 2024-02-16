7a3d8306694f92bc02ca2a76786fe0fb70db87c8
FEWoobIn <hwb0218@gmail.com>
2024-02-16 19:22:47 +0900

    chore: 이미지 드래그를 비활성화 합니다

--- src/components/goods/goods-item-card/goods-item-card.tsx
+++ src/components/goods/goods-item-card/goods-item-card.tsx
@@ -9,7 +9,7 @@ export default function GoodsItemCard({ src, alt = '' }: Props) {
   return (
     <Card className="border-0 overflow-hidden">
       <CardContent className="p-0 aspect-square flex items-center justify-center">
-        <img src={src} alt={alt} className="w-full h-full object-cover" />
+        <img src={src} alt={alt} draggable={false} className="w-full h-full object-cover" />
       </CardContent>
     </Card>
   );
