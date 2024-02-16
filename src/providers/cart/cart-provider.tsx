f2e54c14e4167b467803575817f0e1b48be44976
FEWoobIn <hwb0218@gmail.com>
2024-02-17 05:46:06 +0900

    feat: cart provider 로직을 분리합니다

--- src/providers/cart/cart-provider.tsx
+++ src/providers/cart/cart-provider.tsx
@@ -1,19 +1,15 @@
-import React, { useState, useMemo, createContext, useEffect, useRef, useCallback, PropsWithChildren } from 'react';
-import { storeService } from '@src/lib/firebase/StoreService';
-
-import { useAuthContext } from '@providers/auth';
-
-import { getLocalStorage, setLocalStorage } from '@src/utils/local-storage';
-import calcTotalPrice from '@src/utils/calc-total-price';
+import { useMemo, createContext, PropsWithChildren } from 'react';
+import useCart from './use-cart';
 
 import type { CartGoods } from '@src/lib/firebase/types';
+import useOverlay from './use-overlay';
 
 interface CartContext {
   expanded: boolean;
   cart: CartGoods[];
   checkedGoods: CartGoods[];
   totalPrice: number;
-  onOpenCart: (e: React.MouseEvent) => void;
+  onOpenCart: () => void;
   onAddItemToCart: (item: CartGoods) => void;
   onDeleteItemFromCart: (cartGoodsId: string) => void;
   onToggleCartGoods: (cartGoods: CartGoods) => void;
@@ -35,118 +31,18 @@ export const CartContext = createContext<CartContext>({
 });
 
 export default function CartProvider({ children }: PropsWithChildren) {
-  const storedCart = getLocalStorage({ key: 'cart' }) as CartGoods[];
-  const storedUsers = getLocalStorage({ key: 'auth' });
+  const {
+    cart,
+    checkedGoods,
+    totalPrice,
+    onAddItemToCart,
+    onDeleteItemFromCart,
+    onToggleCartGoods,
+    onAllCheckedGoods,
+    onChangeGoodsCount,
+  } = useCart();
 
-  const [expanded, setExpanded] = useState(false);
-  const [cart, setCart] = useState<CartGoods[]>(storedCart ?? []);
-  const [checkedGoods, setCheckedGoods] = useState<CartGoods[]>(storedCart ?? []);
-  const [totalPrice, setTotalPrice] = useState<number>(calcTotalPrice(storedCart));
-
-  const overlayRef = useRef<HTMLDivElement>(null);
-
-  const { state } = useAuthContext();
-  const { auth } = state;
-
-  useEffect(() => {
-    if (!storedUsers) {
-      setCart([]);
-    }
-  }, [storedUsers]);
-
-  useEffect(() => {
-    const onCloseCart = (e: globalThis.MouseEvent) => {
-      const target = e.target as HTMLElement;
-
-      if (expanded && overlayRef.current?.contains(target)) {
-        document.body.classList.remove('overflow-hidden');
-        setExpanded(false);
-      }
-    };
-
-    window.addEventListener('click', onCloseCart);
-    return () => {
-      window.removeEventListener('click', onCloseCart);
-    };
-  }, [expanded]);
-
-  const onOpenCart = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    document.body.classList.add('overflow-hidden');
-    setExpanded(true);
-  };
-
-  const onAddItemToCart = useCallback(
-    (item: CartGoods) => {
-      const newCartGoods = [item, ...cart];
-
-      setCart(newCartGoods);
-      setLocalStorage({ key: 'cart', value: newCartGoods });
-      setCheckedGoods([...checkedGoods, item]);
-      setTotalPrice(calcTotalPrice(newCartGoods));
-    },
-    [cart, checkedGoods],
-  );
-
-  const onDeleteItemFromCart = useCallback(
-    async (cartGoodsId: string) => {
-      try {
-        await storeService.deleteGoodsToCart(cartGoodsId, auth?.uid);
-
-        const filteredCart = cart.filter((item) => item.id !== cartGoodsId);
-
-        setCart(filteredCart);
-        setLocalStorage({ key: 'cart', value: filteredCart });
-        setCheckedGoods(checkedGoods.filter((goods) => goods.id !== cartGoodsId));
-        setTotalPrice(calcTotalPrice(filteredCart));
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    [cart, checkedGoods, auth?.uid],
-  );
-
-  const onToggleCartGoods = useCallback(
-    (cartGoods: CartGoods) => {
-      const isAlreadyChecked = checkedGoods.some((goods) => goods.id === cartGoods.id);
-
-      const newCheckedGoods = isAlreadyChecked
-        ? checkedGoods.filter((goods) => goods.id !== cartGoods.id)
-        : [...checkedGoods, cartGoods];
-
-      setCheckedGoods(newCheckedGoods);
-      setTotalPrice(calcTotalPrice(newCheckedGoods));
-    },
-    [checkedGoods],
-  );
-
-  const onAllCheckedGoods = useCallback(
-    (checked: HTMLInputElement['checked']) => {
-      if (checked) {
-        setCheckedGoods(storedCart);
-        setTotalPrice(calcTotalPrice(storedCart));
-      } else {
-        setCheckedGoods([]);
-        setTotalPrice(0);
-      }
-    },
-    [storedCart],
-  );
-
-  const onChangeGoodsCount = useCallback(
-    async (cartGoodsId: string, goodsCount: number) => {
-      try {
-        const updatedCartGoods = await storeService.changeGoodsCountFromCart(cartGoodsId, auth?.uid, goodsCount);
-        setCart(updatedCartGoods);
-        setTotalPrice(calcTotalPrice(updatedCartGoods));
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    [auth?.uid],
-  );
-
-  console.log(checkedGoods);
+  const { expanded, onOpenCart, overlayRef } = useOverlay();
 
   const contextValue = useMemo(
     () => ({
@@ -166,6 +62,7 @@ export default function CartProvider({ children }: PropsWithChildren) {
       cart,
       checkedGoods,
       totalPrice,
+      onOpenCart,
       onAddItemToCart,
       onDeleteItemFromCart,
       onToggleCartGoods,
