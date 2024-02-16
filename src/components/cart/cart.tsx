f2c8afac4c447454d3a41d3eda8741898589cbad
FEWoobIn <hwb0218@gmail.com>
2024-02-16 19:20:39 +0900

    fix: 장바구니 체크박스 버그를 수정합니다(#15)

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -1,51 +1,54 @@
-import { useState } from 'react';
-import { storeService } from '@src/lib/firebase/StoreService';
-
 import { useCartContext } from '@providers/cart';
-import { useAuthContext } from '@providers/auth';
 
 import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from '@components/ui/select';
 import { Checkbox } from '@components/ui/checkbox';
+import { Button } from '@components/ui/button';
 import { Li, Ul } from '@components/common/list';
 import Wrapper from '@components/common/wrapper';
 import { GoodsItemCard } from '@components/goods/goods-item-card';
 
 import numberFormat from '@src/utils/number-format';
 
-const ONETOTEN = Array.from({ length: 10 }, (_, i) => String(i + 1));
+import type { CartGoods } from '@src/lib/firebase/types';
+
+const SELECT_ITEMS = Array.from({ length: 10 }, (_, i) => String(i + 1));
 
 export default function Cart() {
-  const [goodsCount, setGoodsCount] = useState('1');
-
-  const { state } = useAuthContext();
-  const { auth } = state;
-  const { expanded, cart, checkedGoods, onDeleteItemFromCart, onAllCheckedGoods, onToggleCartGoods } = useCartContext();
+  const {
+    expanded,
+    cart,
+    checkedGoods,
+    totalPrice,
+    onDeleteItemFromCart,
+    onAllCheckedGoods,
+    onToggleCartGoods,
+    onChangeGoodsCount,
+  } = useCartContext();
 
   const handleAllCheck = (checked: HTMLInputElement['checked']) => {
     onAllCheckedGoods(checked);
   };
 
-  const handleToggleCartGoods = (cartGoodsId: string) => {
-    onToggleCartGoods(cartGoodsId);
+  const handleToggleCartGoods = (cartGoods: CartGoods) => {
+    onToggleCartGoods(cartGoods);
   };
 
-  const handleClickDeleteItem = async (cartGoodsId: string) => {
+  const handleClickDeleteItem = (cartGoodsId: string) => {
     if (window.confirm('상품을 삭제 하시겠습니까?')) {
-      await storeService.deleteGoodsToCart(cartGoodsId, auth?.uid);
       onDeleteItemFromCart(cartGoodsId);
     }
   };
 
-  const handleGoodsCountChange = (value: string) => {
-    setGoodsCount(value);
+  const handleGoodsCountChange = (cartGoodsId: string, goodsCount: string) => {
+    onChangeGoodsCount(cartGoodsId, Number(goodsCount));
   };
 
   return (
     <aside
-      className={`w-96 py-6 px-8 fixed top-[25px] bottom-[25px] ${expanded ? 'right-6' : '-right-96'} bottom-0 bg-white border-l shadow-sm z-[9999] transition-all overflow-auto rounded-md scrollbar-none`}
+      className={`w-96 pt-6 fixed top-[25px] bottom-[25px] ${expanded ? 'right-6' : '-right-96'} bottom-0 bg-white border-l shadow-sm z-[9999] transition-all overflow-auto rounded-md scrollbar-none`}
     >
       <Ul className="flex flex-col items-start gap-y-4">
-        <Wrapper className="mb-3 flex items-center gap-x-2">
+        <Wrapper className="mb-3 px-8 flex items-center gap-x-2">
           <Checkbox
             id="all"
             checked={checkedGoods.length === cart.length && checkedGoods.length > 0}
@@ -57,7 +60,7 @@ export default function Cart() {
           </label>
         </Wrapper>
         {cart?.map((cartGoods) => (
-          <Li className="flex flex-col w-full">
+          <Li className="flex flex-col w-full px-8">
             <Wrapper className="flex">
               <label
                 htmlFor={`goods-${cartGoods.id}`}
@@ -65,8 +68,8 @@ export default function Cart() {
               >
                 <Checkbox
                   id={`goods-${cartGoods.id}`}
-                  checked={checkedGoods.includes(cartGoods.id)}
-                  onClick={() => handleToggleCartGoods(cartGoods.id)}
+                  checked={checkedGoods.some((goods) => goods.id === cartGoods.id)}
+                  onCheckedChange={() => handleToggleCartGoods(cartGoods)}
                   className="absolute left-0 top-0 rounded-md border-slate-100 bg-white h-6 w-6 data-[state=checked]:bg-lime-400 data-[state=checked]:border-none"
                 />
                 <Wrapper className="w-[100px] h-[100px]">
@@ -83,12 +86,16 @@ export default function Cart() {
               </Wrapper>
             </Wrapper>
             <Wrapper className="pt-3 pb-4 flex items-center justify-between">
-              <Select onValueChange={handleGoodsCountChange} value={goodsCount} defaultValue={goodsCount}>
+              <Select
+                defaultValue={cartGoods.goodsCount.toString()}
+                value={cartGoods.goodsCount.toString()}
+                onValueChange={(goodsCount) => handleGoodsCountChange(cartGoods.id, goodsCount)}
+              >
                 <SelectTrigger className="w-[100px]">
-                  <SelectValue>{goodsCount}</SelectValue>
+                  <SelectValue>{cartGoods.goodsCount}</SelectValue>
                 </SelectTrigger>
                 <SelectContent className="min-w-[100px] z-[1000000]">
-                  {ONETOTEN.map((number) => (
+                  {SELECT_ITEMS.map((number) => (
                     <SelectItem key={number} value={number}>
                       {number}
                     </SelectItem>
@@ -99,7 +106,15 @@ export default function Cart() {
             </Wrapper>
           </Li>
         ))}
-        <Wrapper>최종 결제금액</Wrapper>
+        <Wrapper className="sticky bottom-0 bg-white py-6 px-8 w-full border-t border-stone-200 ">
+          <Wrapper className="flex justify-between">
+            <span>최종 결제금액</span>
+            <strong className="text-lg">{numberFormat(totalPrice)}원</strong>
+          </Wrapper>
+          <Wrapper className="pt-3">
+            <Button className="w-full">구매하기</Button>
+          </Wrapper>
+        </Wrapper>
       </Ul>
     </aside>
   );
--- src/providers/cart/cart-provider.tsx
+++ src/providers/cart/cart-provider.tsx
@@ -1,40 +1,52 @@
 import React, { useState, useMemo, createContext, useEffect, useRef, useCallback, PropsWithChildren } from 'react';
+import { storeService } from '@src/lib/firebase/StoreService';
+
+import { useAuthContext } from '@providers/auth';
+
+import { getLocalStorage, setLocalStorage } from '@src/utils/local-storage';
+import calcTotalPrice from '@src/utils/calc-total-price';
 
 import type { CartGoods } from '@src/lib/firebase/types';
-import { getLocalStorage, setLocalStorage } from '@src/utils/local-storage';
 
 interface CartContext {
   expanded: boolean;
   cart: CartGoods[];
-  checkedGoods: CartGoods['id'][];
+  checkedGoods: CartGoods[];
+  totalPrice: number;
   onOpenCart: (e: React.MouseEvent) => void;
   onAddItemToCart: (item: CartGoods) => void;
   onDeleteItemFromCart: (cartGoodsId: string) => void;
-  onToggleCartGoods: (cartGoodsId: string) => void;
+  onToggleCartGoods: (cartGoods: CartGoods) => void;
   onAllCheckedGoods: (checked: HTMLInputElement['checked']) => void;
+  onChangeGoodsCount: (cartGoodsId: string, goodsCount: number) => void;
 }
 
 export const CartContext = createContext<CartContext>({
   expanded: false,
   checkedGoods: [],
   cart: [],
+  totalPrice: 0,
   onOpenCart: () => {},
   onAddItemToCart: () => {},
   onDeleteItemFromCart: () => {},
   onToggleCartGoods: () => {},
   onAllCheckedGoods: () => {},
+  onChangeGoodsCount: () => {},
 });
 
 export default function CartProvider({ children }: PropsWithChildren) {
   const storedCart = getLocalStorage({ key: 'cart' }) as CartGoods[];
   const storedUsers = getLocalStorage({ key: 'auth' });
-  const initCartGoodsId = storedCart.map((cartGoods) => cartGoods.id);
 
   const [expanded, setExpanded] = useState(false);
   const [cart, setCart] = useState<CartGoods[]>(storedCart ?? []);
-  const [checkedGoods, setcheckedGoods] = useState<string[]>(initCartGoodsId);
+  const [checkedGoods, setCheckedGoods] = useState<CartGoods[]>(storedCart ?? []);
+  const [totalPrice, setTotalPrice] = useState<number>(calcTotalPrice(storedCart));
 
   const overlayRef = useRef<HTMLDivElement>(null);
+
+  const { state } = useAuthContext();
+  const { auth } = state;
 
   useEffect(() => {
     if (!storedUsers) {
@@ -66,31 +78,44 @@ export default function CartProvider({ children }: PropsWithChildren) {
 
   const onAddItemToCart = useCallback(
     (item: CartGoods) => {
-      setCart([item, ...cart]);
-      setLocalStorage({ key: 'cart', value: [item, ...cart] });
-      setcheckedGoods([...checkedGoods, item.id]);
+      const newCartGoods = [item, ...cart];
+
+      setCart(newCartGoods);
+      setLocalStorage({ key: 'cart', value: newCartGoods });
+      setCheckedGoods([...checkedGoods, item]);
+      setTotalPrice(calcTotalPrice(newCartGoods));
     },
     [cart, checkedGoods],
   );
 
   const onDeleteItemFromCart = useCallback(
-    (cartGoodsId: string) => {
-      const filteredCart = cart.filter((item) => item.id !== cartGoodsId);
+    async (cartGoodsId: string) => {
+      try {
+        await storeService.deleteGoodsToCart(cartGoodsId, auth?.uid);
 
-      setCart(filteredCart);
-      setLocalStorage({ key: 'cart', value: filteredCart });
-      setcheckedGoods(checkedGoods.filter((id) => id !== cartGoodsId));
+        const filteredCart = cart.filter((item) => item.id !== cartGoodsId);
+
+        setCart(filteredCart);
+        setLocalStorage({ key: 'cart', value: filteredCart });
+        setCheckedGoods(checkedGoods.filter((goods) => goods.id !== cartGoodsId));
+        setTotalPrice(calcTotalPrice(filteredCart));
+      } catch (err) {
+        console.error(err);
+      }
     },
-    [cart, checkedGoods],
+    [cart, checkedGoods, auth?.uid],
   );
 
   const onToggleCartGoods = useCallback(
-    (cartGoodsId: string) => {
-      const checkedGoodsSet = new Set(checkedGoods);
-      checkedGoodsSet.has(cartGoodsId) ? checkedGoodsSet.delete(cartGoodsId) : checkedGoodsSet.add(cartGoodsId);
-      const newCheckedCartGoods = [...checkedGoodsSet];
+    (cartGoods: CartGoods) => {
+      const isAlreadyChecked = checkedGoods.some((goods) => goods.id === cartGoods.id);
 
-      setcheckedGoods(newCheckedCartGoods);
+      const newCheckedGoods = isAlreadyChecked
+        ? checkedGoods.filter((goods) => goods.id !== cartGoods.id)
+        : [...checkedGoods, cartGoods];
+
+      setCheckedGoods(newCheckedGoods);
+      setTotalPrice(calcTotalPrice(newCheckedGoods));
     },
     [checkedGoods],
   );
@@ -98,26 +123,55 @@ export default function CartProvider({ children }: PropsWithChildren) {
   const onAllCheckedGoods = useCallback(
     (checked: HTMLInputElement['checked']) => {
       if (checked) {
-        setcheckedGoods(initCartGoodsId);
+        setCheckedGoods(storedCart);
+        setTotalPrice(calcTotalPrice(storedCart));
       } else {
-        setcheckedGoods([]);
+        setCheckedGoods([]);
+        setTotalPrice(0);
       }
     },
-    [initCartGoodsId],
+    [storedCart],
   );
+
+  const onChangeGoodsCount = useCallback(
+    async (cartGoodsId: string, goodsCount: number) => {
+      try {
+        const updatedCartGoods = await storeService.changeGoodsCountFromCart(cartGoodsId, auth?.uid, goodsCount);
+        setCart(updatedCartGoods);
+        setTotalPrice(calcTotalPrice(updatedCartGoods));
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    [auth?.uid],
+  );
+
+  console.log(checkedGoods);
 
   const contextValue = useMemo(
     () => ({
       expanded,
       cart,
       checkedGoods,
+      totalPrice,
       onOpenCart,
       onAddItemToCart,
       onDeleteItemFromCart,
       onToggleCartGoods,
       onAllCheckedGoods,
+      onChangeGoodsCount,
     }),
-    [expanded, cart, checkedGoods, onAddItemToCart, onDeleteItemFromCart, onToggleCartGoods, onAllCheckedGoods],
+    [
+      expanded,
+      cart,
+      checkedGoods,
+      totalPrice,
+      onAddItemToCart,
+      onDeleteItemFromCart,
+      onToggleCartGoods,
+      onAllCheckedGoods,
+      onChangeGoodsCount,
+    ],
   );
 
   return (
