90785bf50caca2d25883c7fa2c23b6d047973108
FEWoobIn <hwb0218@gmail.com>
2024-02-18 21:01:07 +0900

    chore: query hooks 파일명을 변경합니다

--- src/hooks/use-fetch-goods-by-id-query.ts
+++ src/hooks/use-fetch-goods-by-id-query.ts
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from '@constants/query-keys';
 import { storeService } from '@src/lib/firebase/StoreService';
 
-export default function useFetchGoodsById(productId: string) {
+export default function useFetchGoodsByIdQuery(productId: string) {
   return useQuery({
     queryKey: QUERY_KEYS.GOODS.BY_ID(productId),
     queryFn: () => storeService.getGoodsById(productId),
