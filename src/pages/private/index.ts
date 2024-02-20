5f99958afb2bb329085ffff40f5ea42bb3f65305
FEWoobIn <hwb0218@gmail.com>
2024-02-20 23:56:09 +0900

    feat: 구매 정보 페이지를 추가합니다(#19)

--- src/pages/private/index.ts
+++ src/pages/private/index.ts
@@ -1,3 +1,5 @@
 export { default as MyPage } from './my-page';
 export { default as ConsoleMainPage } from './console/console';
 export { default as ConsoleAddProductPage } from './console/add-product';
+export { default as OrderPage } from './order';
+export { default as OrderListPage } from './order-list';
--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -2,7 +2,7 @@ import { RouteObject } from 'react-router-dom';
 
 import { ProtectedLayout, GlobalNavLayout, ConsoleLayout } from '@components/layout';
 
-import { ConsoleMainPage, ConsoleAddProductPage, MyPage } from '@pages/private';
+import { ConsoleMainPage, ConsoleAddProductPage, MyPage, OrderPage, OrderListPage } from '@pages/private';
 
 import { AUTH_ROUTE_PATHS, CONSOLE_ROUTE_PATHS } from '@constants/routes';
 
@@ -13,7 +13,11 @@ const privateRoutes: RouteObject[] = [
         <GlobalNavLayout />
       </ProtectedLayout>
     ),
-    children: [{ path: AUTH_ROUTE_PATHS.MYPAGE(), element: <MyPage /> }],
+    children: [
+      { path: AUTH_ROUTE_PATHS.MYPAGE(), element: <MyPage /> },
+      { path: AUTH_ROUTE_PATHS.ORDER, element: <OrderPage /> },
+      { path: AUTH_ROUTE_PATHS.ORDER_LIST, element: <OrderListPage /> },
+    ],
   },
   {
     element: (
