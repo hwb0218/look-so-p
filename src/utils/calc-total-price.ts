6c8e149440ea3bfb0757fe3b5cee21e30e712f5e
FEWoobIn <hwb0218@gmail.com>
2024-02-20 22:45:24 +0900

    refactor: 타입 추론을 추가합니다

--- src/utils/calc-total-price.ts
+++ src/utils/calc-total-price.ts
@@ -1,5 +1,5 @@
-import { CartGoods } from '@src/lib/firebase/types';
+import { CartGoods, Product } from '@src/lib/firebase/types';
 
-export default function calcTotalPrice(cart: CartGoods[]) {
+export default function calcTotalPrice(cart: CartGoods[] | Product[]) {
   return cart?.reduce((total, item) => total + item.productPrice * item.goodsCount, 0) ?? 0;
 }
