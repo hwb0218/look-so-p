3ac52a252a81a37743a8d48496e7030296203c43
FEWoobIn <hwb0218@gmail.com>
2024-02-20 22:41:46 +0900

    feat: 다음 우편번호 SDK를 이용해 주소 입력을 구현합니다(#19)

---
+++ src/components/order-form/address-search.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react';
+
+import Wrapper from '@components/common/wrapper';
+import { UseFormReturn } from 'react-hook-form';
+import { OrderFormSchema } from '@src/lib/zod/order-schema';
+
+interface Props {
+  setSearching: React.Dispatch<React.SetStateAction<boolean>>;
+  form: UseFormReturn<OrderFormSchema>;
+}
+
+export default function AddressSearch({ setSearching, form }: Props) {
+  const refElement = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    new daum.Postcode({
+      oncomplete(data) {
+        const { jibunAddress, autoJibunAddress, autoRoadAddress, roadAddress, zonecode: postalCode } = data;
+
+        form.setValue('jibunAddress', jibunAddress || autoJibunAddress);
+        form.setValue('roadAddress', roadAddress || autoRoadAddress);
+        form.setValue('postalCode', postalCode);
+        setSearching(false);
+      },
+      width: '100%',
+      height: '100%',
+    }).embed(refElement.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Wrapper className="fixed top-0 left-0 w-full h-full bg-black/50" onClick={() => setSearching(false)}>
+      <div
+        ref={refElement}
+        className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-4/5 h-4/5 max-w-3xl"
+      />
+    </Wrapper>
+  );
+}
