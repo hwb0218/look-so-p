a2b4d2c0eb3645a1bea6cae44df2dd5cab6e3c08
FEWoobIn <hwb0218@gmail.com>
2024-02-22 03:13:59 +0900

    chore: 결제 페이지의 최종 결제금액을 표시합니다

--- src/components/order-form/shipping-form.tsx
+++ src/components/order-form/shipping-form.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+
+import { storeService } from '@src/lib/firebase/StoreService';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { OrderFormSchema, orderFormSchema } from '@src/lib/zod/order-schema';
@@ -7,19 +10,19 @@ import { OrderFormSchema, orderFormSchema } from '@src/lib/zod/order-schema';
 import { useAuthContext } from '@providers/auth';
 import usePayment from '@hooks/use-payment';
 
+import { AddressSearch } from '.';
+
 import Wrapper from '@components/common/wrapper';
 import { Button } from '@components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@components/ui/form';
 import { Input } from '@components/ui/input';
-import AddressSearch from './address-search';
-
-import { storeService } from '@src/lib/firebase/StoreService';
 
 import calcTotalPrice from '@src/utils/calc-total-price';
+import numberFormat from '@src/utils/number-format';
+
+import { AUTH_ROUTE_PATHS } from '@constants/routes';
 
 import { type CartGoods, type Order } from '@src/lib/firebase/types';
-import { useNavigate } from 'react-router-dom';
-import { AUTH_ROUTE_PATHS } from '@constants/routes';
 
 interface Props {
   checkedGoods: CartGoods[];
@@ -86,7 +89,13 @@ export default function ShippingForm({ checkedGoods, setCart, setTotalPrice }: P
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <Wrapper className="flex items-end pt-6 pb-1 w-full">
           <h2 className="inline-block text-xl">배송지 정보</h2>
-          <Button className="ml-auto">결제하기</Button>
+          <Wrapper className="ml-auto">
+            <strong>최종 결제금액 </strong>
+            <span className="text-lg">
+              <strong>{numberFormat(calcTotalPrice(checkedGoods))}원</strong>
+            </span>
+            <Button className="ml-3">결제하기</Button>
+          </Wrapper>
         </Wrapper>
         <table className="w-full">
           <colgroup>
