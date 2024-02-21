a2b05b395b52a42fa037a724a9ead2d7140b7bc6
FEWoobIn <hwb0218@gmail.com>
2024-02-22 08:58:20 +0900

    feat:  판매자 콘솔 navbar의 로고에 홈 링크버튼을 추가합니다

--- src/components/layout/global-nav-layout/global-nav.tsx
+++ src/components/layout/global-nav-layout/global-nav.tsx
@@ -12,11 +12,14 @@ function GlobalNav() {
 
   return (
     <header className="fixed top-0 left-0 right-0 px-6 py-2 flex justify-between z-10 border-b bg-white/70">
-      <Ul>
+      <Ul className="flex items-center gap-x-6 font-black">
         <Li className="w-14 rounded overflow-hidden">
           <NavLink to={ROUTE_PATHS.HOME}>
             <img src="/logo.svg" />
           </NavLink>
+        </Li>
+        <Li>
+          <span className="text-xl">LookSoPrt</span>
         </Li>
       </Ul>
       <Ul className="gap-x-2">
