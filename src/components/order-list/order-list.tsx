cb167138c670a26aa99279b9cb8c83b4e062c859
FEWoobIn <hwb0218@gmail.com>
2024-02-22 02:52:17 +0900

    feat: 구매자 주문 관련 API, 구매 정보 페이지를 구현합니다(#21)

--- src/components/order-list/order-list.tsx
+++ src/components/order-list/order-list.tsx
@@ -1,3 +1,87 @@
-export default function OrderList() {
-  return <div></div>;
+import { useModalContext } from '@providers/modal';
+
+import { CancelPaymentModal } from '.';
+
+import Wrapper from '@components/common/wrapper';
+import { Li, Ul } from '@components/common/list';
+import { Button } from '@components/ui/button';
+
+import dateFormat from '@src/utils/date-format';
+import numberFormat from '@src/utils/number-format';
+
+import type { OrderList } from '@src/lib/firebase/types';
+
+interface Props {
+  orders: OrderList[];
+}
+export default function OrderList({ orders }: Props) {
+  const { openModal } = useModalContext();
+
+  const handleClickCancelButton = (orderId: string, orderListId: string = '') => {
+    openModal(<CancelPaymentModal orderId={orderId} orderListId={orderListId} />);
+  };
+
+  return (
+    <Wrapper className="max-w-5xl pb-20 mx-auto my-0">
+      <h2 className="pt-10 pb-1 text-xl">주문/배송 조회</h2>
+      <table className="w-full">
+        <colgroup>
+          <col className="w-1/6" />
+          <col />
+          <col className="w-1/12" />
+          <col className="w-1/6" />
+          <col className="w-1/6" />
+        </colgroup>
+        <thead>
+          <tr className="*:p-2 border-t-2 border-b bg-gray-100">
+            <th>주문일자</th>
+            <th>상품</th>
+            <th>수량</th>
+            <th>주문금액</th>
+            <th>상태</th>
+          </tr>
+        </thead>
+        {orders.map(({ orderItems, id, merchantId, createdAt }) => (
+          <tbody key={id}>
+            {orderItems?.map((orderItem, i) => {
+              const firstItem = i === 0;
+              return (
+                <tr key={orderItem.id} className="*:p-4 *:border-b *:text-center [&>:not(:first-child)]:border-l">
+                  {firstItem && (
+                    <td rowSpan={orderItems?.length} className="text-center">
+                      <Ul className="flex-col">
+                        <Li className="text-sm font-bold">{dateFormat(createdAt?.toDate())}</Li>
+                        <Li className="text-sm text-lime-600">{merchantId}</Li>
+                      </Ul>
+                    </td>
+                  )}
+                  <td className="border-l">
+                    <Wrapper className="flex items-center">
+                      <span className="w-[80px] h-[80px]">
+                        <img src={orderItem.thumbnail} alt={orderItem.productName} className="w-full h-full" />
+                      </span>
+                      <span className="ml-4 text-sm">{orderItem.productName}</span>
+                    </Wrapper>
+                  </td>
+                  <td>{orderItem.goodsCount}</td>
+                  <td className="text-lime-600">{numberFormat(orderItem.totalPrice)}원</td>
+                  <td>
+                    <strong className="block mb-1 text-sm">{orderItem.status}</strong>
+                    <Button
+                      type="button"
+                      variant="outline"
+                      onClick={() => handleClickCancelButton(id, orderItem?.id)}
+                      className="h-8 px-2"
+                    >
+                      구매 취소
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        ))}
+      </table>
+    </Wrapper>
+  );
 }
