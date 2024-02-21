559f9cb3629831d28837db46c42978f45acf2914
FEWoobIn <hwb0218@gmail.com>
2024-02-22 03:14:29 +0900

    chore: AddressSearch 컴포넌트 내보내기

--- src/components/order-form/index.ts
+++ src/components/order-form/index.ts
@@ -1,3 +1,4 @@
 export { default as OrderForm } from './order-form';
 export { default as ShippingForm } from './shipping-form';
 export { default as GoodsInfo } from './goods-info';
+export { default as AddressSearch } from './address-search';
