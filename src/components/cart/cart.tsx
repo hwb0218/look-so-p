113d2df52fef5f3df8e4f13597d1388de8bd5f58
FEWoobIn <hwb0218@gmail.com>
2024-02-22 06:37:39 +0900

    style: 장바구니 스타일을 수정합니다

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -54,82 +54,84 @@ export default function Cart() {
   };
 
   return (
-    <aside
-      className={`w-96 fixed top-[25px] bottom-[25px] ${expanded ? 'right-6' : '-right-96'} bottom-0 bg-white z-[9999] transition-all rounded-md`}
+    <Wrapper
+      className={`${expanded ? '-translate-x-full' : 'translate-x-full'} transform-gpu p-5 h-screen w-96 fixed top-0 bottom-0 left-full z-[9999] transition-all duration-500`}
     >
-      <Wrapper className="relative h-full flex flex-col">
-        <Wrapper className="mb-3 px-8 py-4 flex items-center gap-x-2">
-          <Checkbox
-            id="all"
-            checked={checkedGoods.length === cart.length && checkedGoods.length > 0}
-            onCheckedChange={handleAllCheck}
-            className="rounded-md border-slate-100 bg-white h-6 w-6 data-[state=checked]:bg-lime-400 data-[state=checked]:border-none"
-          />
-          <label htmlFor="all" className="text-sm">
-            전체
-          </label>
-        </Wrapper>
-        <Ul className="mb-[80px] pb-[40px] flex-col justify-normal overflow-y-scroll scrollbar-none">
-          {cart?.map((cartGoods) => (
-            <Li key={cartGoods.id} className="flex flex-col w-full pb-8 px-8">
-              <Wrapper className="flex">
-                <label
-                  htmlFor={`goods-${cartGoods.id}`}
-                  className="relative leading-none text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 select-none"
-                >
-                  <Checkbox
-                    id={`goods-${cartGoods.id}`}
-                    checked={checkedGoods.some((goods) => goods.id === cartGoods.id)}
-                    onCheckedChange={() => handleToggleCartGoods(cartGoods)}
-                    className="absolute left-0 top-0 rounded-md border-slate-100 bg-white h-6 w-6 data-[state=checked]:bg-lime-400 data-[state=checked]:border-none"
-                  />
-                  <Wrapper className="w-[100px] h-[100px]">
-                    <GoodsItemCard src={cartGoods.thumbnail} alt={cartGoods.productName} />
-                  </Wrapper>
-                </label>
-                <Wrapper className="ml-3 w-full flex flex-col items-start">
-                  <Wrapper className="ml-auto">
-                    <button onClick={() => handleClickDeleteItem(cartGoods.id)}>
-                      <img src="/close_MD.svg" alt="close" />
-                    </button>
-                  </Wrapper>
-                  <span>{cartGoods.productName}</span>
-                </Wrapper>
-              </Wrapper>
-              <Wrapper className="pt-3 pb-4 flex items-center justify-between">
-                <Select
-                  defaultValue={cartGoods.goodsCount.toString()}
-                  value={cartGoods.goodsCount.toString()}
-                  onValueChange={(goodsCount) => handleGoodsCountChange(cartGoods, goodsCount)}
-                >
-                  <SelectTrigger className="w-[100px]">
-                    <SelectValue>{cartGoods.goodsCount}</SelectValue>
-                  </SelectTrigger>
-                  <SelectContent className="min-w-[100px] z-[1000000]">
-                    {SELECT_ITEMS.map((number) => (
-                      <SelectItem key={number} value={number}>
-                        {number}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <strong>{numberFormat(cartGoods.productPrice * cartGoods.goodsCount)}원</strong>
-              </Wrapper>
-            </Li>
-          ))}
-          <Wrapper className="absolute bottom-0 left-0 right-0 bg-white py-6 px-8 w-full rounded-b-md border-t border-stone-200 ">
-            <Wrapper className="flex justify-between">
-              <span>최종 결제금액</span>
-              <strong className="text-lg">{numberFormat(totalPrice)}원</strong>
-            </Wrapper>
-            <Wrapper className="pt-3">
-              <Button className="w-full" onClick={handleClickPayment}>
-                주문하기
-              </Button>
-            </Wrapper>
+      <aside className="relative h-full w-full rounded-md bg-white">
+        <Wrapper className="h-full w-full flex flex-col">
+          <Wrapper className="mb-3 px-8 py-4 flex items-center gap-x-2">
+            <Checkbox
+              id="all"
+              checked={checkedGoods.length === cart.length && checkedGoods.length > 0}
+              onCheckedChange={handleAllCheck}
+              className="rounded-md border-slate-100 bg-white h-6 w-6 data-[state=checked]:bg-lime-400 data-[state=checked]:border-none"
+            />
+            <label htmlFor="all" className="text-sm">
+              전체
+            </label>
           </Wrapper>
-        </Ul>
-      </Wrapper>
-    </aside>
+          <Ul className="mb-[80px] pb-[40px] flex-col justify-normal overflow-y-scroll scrollbar-none">
+            {cart?.map((cartGoods) => (
+              <Li key={cartGoods.id} className="flex flex-col w-full pb-8 px-8">
+                <Wrapper className="flex">
+                  <label
+                    htmlFor={`goods-${cartGoods.id}`}
+                    className="relative leading-none text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 select-none"
+                  >
+                    <Checkbox
+                      id={`goods-${cartGoods.id}`}
+                      checked={checkedGoods.some((goods) => goods.id === cartGoods.id)}
+                      onCheckedChange={() => handleToggleCartGoods(cartGoods)}
+                      className="absolute left-0 top-0 rounded-md border-slate-100 bg-white h-6 w-6 data-[state=checked]:bg-lime-400 data-[state=checked]:border-none"
+                    />
+                    <Wrapper className="w-[100px] h-[100px]">
+                      <GoodsItemCard src={cartGoods.thumbnail} alt={cartGoods.productName} />
+                    </Wrapper>
+                  </label>
+                  <Wrapper className="ml-3 w-full flex flex-col items-start">
+                    <Wrapper className="ml-auto">
+                      <button onClick={() => handleClickDeleteItem(cartGoods.id)}>
+                        <img src="/close_MD.svg" alt="close" />
+                      </button>
+                    </Wrapper>
+                    <span>{cartGoods.productName}</span>
+                  </Wrapper>
+                </Wrapper>
+                <Wrapper className="pt-3 pb-4 flex items-center justify-between">
+                  <Select
+                    defaultValue={cartGoods.goodsCount.toString()}
+                    value={cartGoods.goodsCount.toString()}
+                    onValueChange={(goodsCount) => handleGoodsCountChange(cartGoods, goodsCount)}
+                  >
+                    <SelectTrigger className="w-[100px]">
+                      <SelectValue>{cartGoods.goodsCount}</SelectValue>
+                    </SelectTrigger>
+                    <SelectContent className="min-w-[100px] z-[1000000]">
+                      {SELECT_ITEMS.map((number) => (
+                        <SelectItem key={number} value={number}>
+                          {number}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <strong>{numberFormat(cartGoods.productPrice * cartGoods.goodsCount)}원</strong>
+                </Wrapper>
+              </Li>
+            ))}
+            <Wrapper className="absolute bottom-0 left-0 right-0 bg-white py-6 px-8 w-full rounded-b-md border-t border-stone-200 ">
+              <Wrapper className="flex justify-between">
+                <span>최종 결제금액</span>
+                <strong className="text-lg">{numberFormat(totalPrice)}원</strong>
+              </Wrapper>
+              <Wrapper className="pt-3">
+                <Button className="w-full" onClick={handleClickPayment}>
+                  주문하기
+                </Button>
+              </Wrapper>
+            </Wrapper>
+          </Ul>
+        </Wrapper>
+      </aside>
+    </Wrapper>
   );
 }
