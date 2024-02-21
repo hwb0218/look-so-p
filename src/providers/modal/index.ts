4f3caacced6240ce5fc076391c155d7ebb58f884
FEWoobIn <hwb0218@gmail.com>
2024-02-22 02:29:35 +0900

    feat: 합성 컴포넌트를 적용한 모달을 구현합니다(#20)

--- src/providers/modal/index.ts
+++ src/providers/modal/index.ts
@@ -1,2 +1,3 @@
 export { default as ModalProvider } from './modal-provider';
 export { default as useModalContext } from './use-modal-context';
+export { default as ModalPortal } from './modal-portal';
