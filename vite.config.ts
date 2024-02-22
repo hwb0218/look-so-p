78518e14507cd7b57087f86582f21bb21d5206b1
FEWoobIn <hwb0218@gmail.com>
2024-02-23 05:27:26 +0900

    chore: 번들 분석기를 설치합니다

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import webfontDownload from 'vite-plugin-webfont-dl';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     webfontDownload(['https://github.com/orioncactus/pretendard/tree/v1.3.9/packages/pretendard/dist']),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true,
+      template: 'treemap',
+    }) as PluginOption,
   ],
 });
