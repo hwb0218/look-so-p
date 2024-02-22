17358d710aa7f780ebc7d48a7af82b4201577bbf
FEWoobIn <hwb0218@gmail.com>
2024-02-22 22:53:24 +0900

    feat: 주문 상태를 업데이트하는 Selectbox UI 구현(#22)
    - 상품 상태 변경에 optimistic update를 적용합니다.

--- src/components/console/console-order-management/console-order-management.tsx
+++ src/components/console/console-order-management/console-order-management.tsx
@@ -1,17 +1,115 @@
-import { Ul } from '@components/common/list';
+import { useState } from 'react';
 
-import { type OrderList } from '@src/lib/firebase/types';
+import { useUpdateOrderManageMentMutation } from '@hooks/use-fetch-order-management-query';
+
+import { Li, Ul } from '@components/common/list';
+import Wrapper from '@components/common/wrapper';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@components/ui/select';
+
+import dateFormat from '@src/utils/date-format';
+import numberFormat from '@src/utils/number-format';
+
+import { Status, type OrderList } from '@src/lib/firebase/types';
 
 interface Props {
   orders: OrderList[];
+  sellerId: string;
 }
 
-export default function ConsoleOrderManagement({ orders }: Props) {
-  console.log(orders);
+export default function ConsoleOrderManagement({ orders, sellerId }: Props) {
+  const [statusSelectView, setStatusSelectView] = useState('');
+
+  const { mutate } = useUpdateOrderManageMentMutation(sellerId);
+
+  const handleChange = (status: Status, orderId: string, orderListId: string = '') => {
+    mutate({ orderId, orderListId, status });
+    setStatusSelectView('');
+  };
+
+  const handleClickStatusSelectView = (orderListId: string = '') => {
+    if (statusSelectView !== '') {
+      setStatusSelectView('');
+    } else {
+      setStatusSelectView(orderListId);
+    }
+  };
 
   return (
-    <Ul className="flex-col items-start space-y-4">
+    <Wrapper className="flex-col items-start space-y-4">
       <h1 className="px-4 text-2xl font-bold">주문 관리</h1>
-    </Ul>
+      <table className="w-full">
+        <colgroup>
+          <col className="w-1/6" />
+          <col />
+          <col className="w-1/12" />
+          <col className="w-[10%]" />
+          <col className="w-1/6" />
+        </colgroup>
+        <thead>
+          <tr className="*:p-2 border-t-2 border-b bg-gray-100">
+            <th>주문일자</th>
+            <th>상품</th>
+            <th>수량</th>
+            <th>주문금액</th>
+            <th>상태</th>
+          </tr>
+        </thead>
+        {orders?.map(({ orderItems, id, merchantId, createdAt, uid }) => (
+          <tbody key={id}>
+            {orderItems?.map((orderItem, i) => {
+              const firstItem = i === 0;
+              return (
+                <tr key={orderItem.id} className="*:p-4 *:border-b *:text-center [&>:not(:first-child)]:border-l">
+                  {firstItem && (
+                    <td rowSpan={orderItems?.length} className="text-center">
+                      <Ul className="flex-col text-sm">
+                        <Li className="font-bold">{dateFormat(createdAt?.toDate())}</Li>
+                        <Li className="text-lime-600">{merchantId}</Li>
+                        <Li>{`uid: ${uid}`}</Li>
+                      </Ul>
+                    </td>
+                  )}
+                  <td className="max-w-[110px] border-l">
+                    <Wrapper className="flex items-center">
+                      <span className="w-[80px] h-[80px] min-w-[80px] min-h-[80px]">
+                        <img src={orderItem.thumbnail} alt={orderItem.productName} className="w-full h-full" />
+                      </span>
+                      <span className="ml-4 text-sm truncate">{orderItem.productName}</span>
+                    </Wrapper>
+                  </td>
+                  <td>{orderItem.goodsCount}</td>
+                  <td className="text-lime-600">{numberFormat(orderItem.totalPrice)}원</td>
+                  <td className="h-full">
+                    <strong
+                      className="inline-block p-2 text-sm cursor-pointer"
+                      onClick={() => handleClickStatusSelectView(orderItem.id)}
+                    >
+                      {orderItem.status}
+                    </strong>
+                    {statusSelectView === orderItem.id && (
+                      <Select
+                        defaultValue={orderItem.status}
+                        onValueChange={(value: Status) => handleChange(value, id, orderItem.id)}
+                      >
+                        <SelectTrigger className="w-full">
+                          <SelectValue>{orderItem.status}</SelectValue>
+                        </SelectTrigger>
+                        <SelectContent className="min-w-[100px] z-[1000000]">
+                          {Object.entries(Status).map(([key, value]) => (
+                            <SelectItem key={key} value={value}>
+                              {value}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        ))}
+      </table>
+    </Wrapper>
   );
 }
--- src/hooks/use-fetch-order-management-query.ts
+++ src/hooks/use-fetch-order-management-query.ts
@@ -1,8 +1,12 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useMutation } from '@tanstack/react-query';
 import { storeService } from '@src/lib/firebase/StoreService';
 import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore';
 
+import { queryClient } from '@src/main';
+
 import { QUERY_KEYS } from '@constants/query-keys';
+
+import { Status } from '@src/lib/firebase/types';
 
 export default function useFetchOrderManagementQuery({
   sellerId,
@@ -30,4 +34,16 @@ export default function useFetchOrderManagementQuery({
     isFetchingNextPage: res.isFetchingNextPage,
     isFetched: res.isFetched,
   };
+}
+
+export function useUpdateOrderManageMentMutation(sellerId: string) {
+  const queryKey = QUERY_KEYS.CONSOLE.ORDER_MANAGEMENT(sellerId);
+
+  return useMutation({
+    mutationFn: ({ orderId, orderListId, status }: { orderId: string; orderListId: string; status: Status }) =>
+      storeService.updateOrderListStatus(orderId, orderListId, status),
+    onSettled: async () => {
+      return await queryClient.invalidateQueries({ queryKey });
+    },
+  });
 }
