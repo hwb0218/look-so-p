f29e2e2bbb00f6632847ffdbac2c9f5b0c0745ab
FEWoobIn <hwb0218@gmail.com>
2024-02-23 05:36:11 +0900

    feat: 스피너 컴포넌트를 추가합니다

---
+++ src/components/common/spinner.tsx
@@ -0,0 +1,9 @@
+import Wrapper from './wrapper';
+
+export default function Loading() {
+  return (
+    <Wrapper className="absolute top-4 left-4">
+      <img src="/spinner.gif" alt="로딩" width="60%" />
+    </Wrapper>
+  );
+}
