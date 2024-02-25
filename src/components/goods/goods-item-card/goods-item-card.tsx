3d47261fb4cf2fae8454287c1c4b406e33579d46
FEWoobIn <hwb0218@gmail.com>
2024-02-26 01:55:28 +0900

    feat: 이미지 최적화 - 이미지 lazy loading을 적용합니다(#27)

--- src/components/goods/goods-item-card/goods-item-card.tsx
+++ src/components/goods/goods-item-card/goods-item-card.tsx
@@ -1,5 +1,7 @@
 import Wrapper from '@components/common/ui/wrapper';
 import { Card, CardContent } from '@components/ui/card';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 interface Props {
   src: string;
@@ -7,11 +9,23 @@ interface Props {
 }
 
 export default function GoodsItemCard({ src, alt = '' }: Props) {
+  const [isVisible, setIsVisible] = useState(false);
+  const { ref, inView } = useInView({
+    rootMargin: '200px',
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setIsVisible(true);
+    }
+  }, [inView]);
+
   return (
-    <Wrapper className="pt-[100%] relative w-full">
-      <Card className="absolute inset-0 border-0 overflow-hidden">
-        <CardContent className="p-0 aspect-square flex items-center justify-center">
-          <img src={src} alt={alt} draggable={false} className="w-full h-full" />
+    <Wrapper ref={ref} className="pt-[100%] relative w-fullrounded-lg">
+      <Card className="absolute inset-0 border-0 shadow-none bg-muted overflow-hidden">
+        <CardContent className="p-0 flex items-center justify-center bg-transparent">
+          {isVisible && <img src={src} alt={alt} draggable={false} className="w-full h-full" />}
         </CardContent>
       </Card>
     </Wrapper>
