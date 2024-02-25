4573784a42dcbd314c2c8f15cc18a8ec0067b162
FEWoobIn <hwb0218@gmail.com>
2024-02-26 04:38:41 +0900

    chore: 스켈레톤 ui에 title을 추가합니다

--- src/components/goods/goods-for-main/goods-skeleton.tsx
+++ src/components/goods/goods-for-main/goods-skeleton.tsx
@@ -6,15 +6,15 @@ import { Skeleton } from '@components/ui/skeleton';
 export default function GoodsSkeleton() {
   return (
     <>
-      {[...Array(2)].map((item) => (
-        <section key={item} className="mb-24">
+      {['클렌징', '토너/패드'].map((title) => (
+        <section key={title} className="mb-24">
           <div className="mx-1 mb-2 flex justify-between items-center">
-            <Skeleton className="w-[110px] h-[32px]" />
+            <h3 className="text-2xl">{title}</h3>
             <span className="cursor-pointer">+ More</span>
           </div>
           <Ul className="justify-normal items-baseline grid grid-cols-4 gap-x-5">
-            {[...Array(4)].map((item) => (
-              <Li key={item} className="w-full h-full">
+            {Array.from({ length: 4 }, (_, i) => i).map((_, i) => (
+              <Li key={i} className="w-full h-full">
                 <GoodsItemSkeletonCard />
                 <Skeleton className="mt-1 max-w-[270px] w-full h-[20px]" />
                 <Skeleton className="mt-1 max-w-[90px] w-full h-[24px]" />
