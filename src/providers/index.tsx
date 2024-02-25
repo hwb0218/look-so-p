4065e2d6827b6a98b6f424159e8911c32a3d3aba
FEWoobIn <hwb0218@gmail.com>
2024-02-26 04:40:21 +0900

    fix: Providers에 ModalPortal 컴포넌트를 추가합니다

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './auth';
-import { ModalProvider } from './modal';
+import { ModalPortal, ModalProvider } from './modal';
 import { CartProvider } from './cart';
 
 export default function Providers({ children }: PropsWithChildren) {
@@ -11,6 +11,7 @@ export default function Providers({ children }: PropsWithChildren) {
       <AuthProvider>
         <ModalProvider>
           <CartProvider>{children}</CartProvider>
+          <ModalPortal />
         </ModalProvider>
       </AuthProvider>
     </HelmetProvider>
