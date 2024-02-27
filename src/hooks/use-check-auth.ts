82430958ab8bc04f49e6d06dc340b05d9d7c7f46
FEWoobIn <hwb0218@gmail.com>
2024-02-28 06:36:41 +0900

    refactor:  메모리 누수를 방지합니다

--- src/hooks/use-check-auth.ts
+++ src/hooks/use-check-auth.ts
@@ -7,21 +7,27 @@ import { getLocalStorage } from '@src/utils/local-storage';
 import { useAuthContext } from '@providers/auth';
 import { authService } from '@src/lib/firebase/AuthService';
 
+import type { User } from '@src/lib/firebase/types';
+
 export default function useCheckAuth() {
   const [accessUid, setAccessUid] = useState(getLocalStorage({ key: 'auth' }));
 
   const { setAuth } = useAuthContext();
-
+  // user 인증정보 아직 남아있으면 새로고침 시 로그인 상태 유지해줌
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (!user) {
         setAccessUid('');
       } else {
         setAccessUid(getLocalStorage({ key: 'auth' }));
       }
     });
-  }, []);
 
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  // 나쁜놈이 로컬날리면 다시 복구함
   useEffect(() => {
     const fetchUser = async () => {
       try {
@@ -41,5 +47,5 @@ export default function useCheckAuth() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accessUid]);
 
-  return accessUid;
+  return accessUid as User;
 }
