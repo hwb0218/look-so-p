01176a2035d57dd41b7988e70994a7e37f8700c6
FEWoobIn <hwb0218@gmail.com>
2024-02-29 07:40:46 +0900

    style: 로그인 폼 스타일을 수정합니다

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -46,48 +46,52 @@ function Login() {
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="h-screen relative flex justify-center items-center flex-col space-y-4"
-      >
-        <Wrapper className="relative p-6 min-w-64 w-1/3 border rounded-md ring-2 ring-ring ring-offset-2">
-          <h1 className="text-2xl text-center font-bold">LookSoPrt</h1>
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem className="mt-2">
-                <FormLabel>아이디</FormLabel>
-                <FormControl>
-                  <Input placeholder="아이디(이메일)" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem className="mt-2">
-                <FormLabel>비밀번호</FormLabel>
-                <FormControl>
-                  <Input placeholder="비밀번호(영문, 숫자, 특수문자 조합)" type="password" {...field} />
-                </FormControl>
-                <FormMessage className="flex-none" />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full mt-5">
-            로그인
-          </Button>
-          <NavLink to={ROUTE_PATHS.HOME} className="absolute top-2 right-2">
-            <img src="/close_MD.svg" alt="close" className="text-black" />
-          </NavLink>
-        </Wrapper>
-      </form>
-    </Form>
+    <Wrapper className="max-w-sm w-full fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex justify-center items-center flex-col space-y-4">
+          <Wrapper className="w-full relative p-6 border rounded-md ring-2 ring-ring ring-offset-2">
+            <h1 className="text-2xl text-center font-bold">LookSoPrt</h1>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="mt-2">
+                  <FormLabel>아이디</FormLabel>
+                  <FormControl>
+                    <Input placeholder="아이디(이메일)" className="w-full" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="mt-2">
+                  <FormLabel>비밀번호</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="비밀번호(영문, 숫자, 특수문자 조합)"
+                      type="password"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="flex-none" />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full mt-5">
+              로그인
+            </Button>
+            <NavLink to={ROUTE_PATHS.HOME} className="absolute top-2 right-2">
+              <img src="/close_MD.svg" alt="close" className="text-black" />
+            </NavLink>
+          </Wrapper>
+        </form>
+      </Form>
+    </Wrapper>
   );
 }
 export default Login;
