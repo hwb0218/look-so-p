e26782ab72cd2e6a6a5052f3c17c3b9e70ee5095
FEWoobIn <hwb0218@gmail.com>
2024-03-01 05:07:22 +0900

    chore: App

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import App from './app';
 
 import Providers from './providers';
 
 import Spinner from '@components/common/spinner/spinner';
 
-import routes from '@routes/routes';
-
 import './index.css';
-
-const router = createBrowserRouter(routes);
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -38,7 +35,7 @@ function main() {
       <QueryClientProvider client={queryClient}>
         <Providers>
           <Suspense fallback={<Spinner />}>
-            <RouterProvider router={router} />
+            <App />
           </Suspense>
         </Providers>
         <ReactQueryDevtools initialIsOpen={false} />
