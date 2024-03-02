a85841d277e99770754612a0af7026983c94dc16
FEWoobIn <hwb0218@gmail.com>
2024-03-03 02:13:29 +0900

    style: 메인 상품 스켈레톤 UI 스타일을 수정합니다

--- src/components/goods/goods-for-main/goods-skeleton.tsx
+++ src/components/goods/goods-for-main/goods-skeleton.tsx
@@ -6,15 +6,15 @@ import { Skeleton } from '@components/ui/skeleton';
 export default function GoodsSkeleton() {
   return (
     <>
-      {['클렌징', '토너/패드'].map((title) => (
+      {['클렌징', '토너/패드', '로션/크림', '에센스/앰플', '선케어'].map((title) => (
         <section key={title} className="mb-24">
           <div className="mx-1 mb-2 flex justify-between items-center">
             <h3 className="text-2xl">{title}</h3>
             <span className="cursor-pointer">+ More</span>
           </div>
           <Ul className="justify-normal items-baseline grid grid-cols-4 gap-x-5">
-            {Array.from({ length: 4 }, (_, i) => i).map((_, i) => (
-              <Li key={i} className="w-full h-full">
+            {Array.from({ length: 4 }, (_, i) => i).map((item) => (
+              <Li key={item} className="w-full h-full">
                 <GoodsItemSkeletonCard />
                 <Skeleton className="mt-1 max-w-[270px] w-full h-[20px]" />
                 <Skeleton className="mt-1 max-w-[90px] w-full h-[24px]" />
