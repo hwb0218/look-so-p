d89620eced9085b7843d81ca06a2cb1f0e8c30ff
FEWoobIn <hwb0218@gmail.com>
2024-03-14 21:58:50 +0900

    refactor: 빌드 옵션을 변경합니다

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,28 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 import webfontDownload from 'vite-plugin-webfont-dl';
 import { visualizer } from 'rollup-plugin-visualizer';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            const module = id.split('node_modules/').pop().split('/')[0];
+            return `vendor/${module}`;
+          }
+        },
+      },
+    },
   },
   plugins: [
     react(),
